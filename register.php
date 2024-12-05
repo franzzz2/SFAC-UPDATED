450842f85e4300ee5dd9426d6ab8fab2539e1cef
franzzz2 <151380564+franzzz2@users.noreply.github.com>
2024-12-05 08:26:19 +0800

    Update register.php

--- register.php
+++ register.php
@@ -33,22 +33,20 @@ if(isset($_POST['signUp'])){
 }
 
 if(isset($_POST['signIn'])){
-   $email=$_POST['email'];
-   $password=$_POST['password'];
-   $password=md5($password) ;
-   
-   $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
-   $result=$conn->query($sql);
-   if($result->num_rows>0){
-    session_start();
-    $row=$result->fetch_assoc();
-    $_SESSION['email']=$row['email'];
-    header("Location: homepage.php");
-    exit();
-   }
-   else{
-    echo "Not Found, Incorrect Email or Password";
-   }
-
-}
-?>
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $password=md5($password);
+    
+    $sql="SELECT * FROM users WHERE email='$email' and password='$password'";
+    $result=$conn->query($sql);
+    if($result->num_rows>0){
+        session_start();
+        $row=$result->fetch_assoc();
+        $_SESSION['email']=$row['email'];
+        header("Location: homepage.php");
+        exit();
+    }
+    else{
+        echo "<script>alert('Not Found, Incorrect Email or Password'); window.location.href='index.php';</script>";
+    }
+ }
